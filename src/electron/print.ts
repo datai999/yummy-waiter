4a501b250ed863816dab20d03724e2e803e12d59
rey <datai28599@gmail.com>
2024-12-20 11:38:38 -0800

    D:print by command

--- src/electron/print.ts
+++ src/electron/print.ts
@@ -1,11 +1,13 @@
 
 const remote = require('@electron/remote');
 
-
 let options = {
-  silent: false,
+  // deviceName: "Microsoft Print to PDF",
+  // deviceName: "Microsoft XPS Document Writer",
+  deviceName: "RP-630(P)",
+  silent: true,
   printBackground: true,
-  color: false,
+  color: true,
   margin: {
     marginType: 'printableArea'
   },
@@ -17,16 +19,37 @@ let options = {
   footer: 'Footer of the Page'
 }
 
+const printByCommand = true;
+
 document.getElementById('current')!.addEventListener('click', (event) => {
   console.log('Print click');
 
+  if (printByCommand) {
+    printByCommandService();
+    console.log('Print done');
+    return;
+  }
+
   let win = remote.getCurrentWindow();
-
-  win.webContents.print(options, (success: boolean, failureReason: any) => {
-    if (!success) console.log(failureReason);
-
+  win.webContents.print(options, function (success: boolean, failureReason: any) {
+    if (!success)
+      console.log(failureReason);
     console.log('Print Initiated');
   });
 
   console.log('Print done');
 });
+
+const printByCommandService = () => {
+  var fs = require('fs');
+  var printString = "whatever text you need to print with optional ascii commands";
+  var printer = "FILE";
+
+  const date = new Date();
+  const fileName = date.getDate() + "_" + date.getHours() + "" + date.getMinutes() + "" + date.getSeconds();
+  var tmpFileName = `.\\print\\${fileName}.prn`;
+  fs.writeFileSync(tmpFileName, printString, "utf8");
+
+  var child = require('child_process').exec;
+  child(`print /d:${printer} ${tmpFileName} \\print ${fileName}`)
+}
